682feedd8ab9844fb67f0f125f79955de4dd4619
Elio Bischof <eliobischof@gmail.com>
2019-12-13 11:01:26 +0100

    docs: fix provision script

--- examples/k8s/static/provision.sh
+++ examples/k8s/static/provision.sh
@@ -3,4 +3,5 @@
 adduser orbiter
 cp -r ~/.ssh /home/orbiter/.ssh
 chown -R 1000:1000 /home/orbiter/.ssh
-echo orbiter	ALL=(ALL)	NOPASSWD: ALL >> /etc/sudoers
+chmod 600 /etc/sudoers
+echo "orbiter	ALL=(ALL)	NOPASSWD: ALL" >> /etc/sudoers
