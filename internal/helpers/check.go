db63db59dbbcc7d8ed01f70c20788b788e028093
Elio Bischof <eliobischof@gmail.com>
2020-03-31 08:08:29 +0200

    fix: enable tcp healthchecks

--- internal/helpers/check.go
+++ internal/helpers/check.go
@@ -4,25 +4,37 @@ import (
 	"crypto/tls"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/AppsFlyer/go-sundheit/checks"
 	"github.com/pkg/errors"
 )
 
-
 func Check(url string, status int) (string, error) {
-	msg, err := checks.Must(checks.NewHTTPCheck(checks.HTTPCheckConfig{
-		CheckName:      "check",
+
+	urlParts := strings.Split(url, "://")
+	if urlParts[0]== "tcp"{
+		return check(checks.NewPingCheck("tcp",checks.NewDialPinger("tcp", urlParts[1]),2*time.Second))
+	}
+
+	return check(checks.NewHTTPCheck(checks.HTTPCheckConfig{
+		CheckName:      "http",
 		Timeout:        1 * time.Second,
 		URL:            url,
 		ExpectedStatus: status,
 		Client: &http.Client{
 			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
 			},
 		},
-	})).Execute()
+	}))
+}
+
+func check(check checks.Check, err error) (string, error){
+	msg, err := checks.Must(check, err).Execute()
 	message := msg.(string)
 	return message, errors.Wrap(err, fmt.Sprintf("%s", message))
 }
