34e1ae4c9a2669c049519932484a3584d5f00294
Elio Bischof <eliobischof@gmail.com>
2020-04-16 15:34:39 +0200

    chore: reenable skaffold debug mode

--- skaffold/build.sh
+++ skaffold/build.sh
@@ -5,14 +5,14 @@ set -e
 rm -rf ./artifacts/*
 
 export CGO_ENABLED=0
-go run ./cmd/gen-executables/*.go --version "${IMAGE}" --commit "$(git log --pretty=format:'%h' -n 1)" --debug $DEBUG --orbctl ./artifacts
+go run ./cmd/gen-executables/*.go --version "${IMAGE}" --commit "$(git log --pretty=format:'%h' -n 1)" "--debug=$DEBUG" --orbctl ./artifacts
 
 TARGET=prod
 if [[ "$DEBUG" == "true" ]]; then
   TARGET=build
 fi
 
-docker build --tag $IMAGE --build-arg DEBUG --target $TARGET .
+docker build --tag $IMAGE --target $TARGET .
 [[ $PUSH_IMAGE=true ]] && docker push $IMAGE
 rm -rf ./artifacts/*
 
