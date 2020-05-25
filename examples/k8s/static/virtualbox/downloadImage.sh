e84893b2dab55c35da59bcf8b74a946ba44ff25d
Stefan Benz <stefan@caos.ch>
2020-05-25 13:12:25 +0200

    docs(example): corrected image in virtualbox example

--- examples/k8s/static/virtualbox/downloadImage.sh
+++ examples/k8s/static/virtualbox/downloadImage.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 tmpFolder=$1
-imageName=$2
-imageURL=$3
+
+imageURL=http://mirror.init7.net/centos/7.8.2003/isos/x86_64/CentOS-7-x86_64-Minimal-2003.iso
 
 imageFolder=${tmpFolder}/images
 mkdir -p ${imageFolder}
 
-imagePath=${imageFolder}/${imageName}
+imagePath=${imageFolder}/centos.iso
 wget -O ${imagePath} ${imageURL}
--- examples/k8s/static/virtualbox/exampleClusterStart.sh
+++ examples/k8s/static/virtualbox/exampleClusterStart.sh
@@ -2,12 +2,7 @@
 publicKeyFile=$1
 tmpFolder=$2
 
-imageURL=http://mirror.init7.net/centos/7.7.1908/isos/x86_64/CentOS-7-x86_64-Minimal-1908.iso
-imageName=centos.iso
-
-./downloadImage.sh ${tmpFolder} ${imageName} ${imageURL}
-
 for MACHINE in  master1 worker1
 do
-    ./machine.sh $MACHINE ks.cfg ${publicKeyFile} ${tmpFolder} ${imageName}
+    ./machine.sh $MACHINE ks.cfg ${publicKeyFile} ${tmpFolder} centos.iso
 done
