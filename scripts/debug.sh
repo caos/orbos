23c538e75332f0a688f8d11821192af4461808a2
Elio Bischof <eliobischof@gmail.com>
2020-06-04 16:48:08 +0200

    Update debug.sh

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-go run ./cmd/gen-executables/*.go --debug --version $(git rev-parse --abbrev-ref HEAD | sed -e "s/heads\///") --commit $(git rev-parse HEAD) --orbctl /tmp/orbctldev 
+go run ./cmd/gen-executables/*.go --debug --version $(git rev-parse --abbrev-ref HEAD | sed -e "s/heads\///") --commit $(git rev-parse HEAD) --orbctl ./artifacts 
+CGO_ENABLED=0 GOOS=linux go build -o ./artifacts/gen-charts  cmd/gen-charts/*.go
 dlv exec --api-version 2 --headless --listen 127.0.0.1:5000 /tmp/orbctldev/orbctl-Linux-x86_64 -- $@
