0cfdbd2d157d94daed5d07fe29835d6146ce1d6c
Elio Bischof <eliobischof@gmail.com>
2020-06-16 16:52:29 +0200

    fix: ensure node software in the order given

--- internal/operator/nodeagent/ensure.go
+++ internal/operator/nodeagent/ensure.go
@@ -96,9 +96,16 @@ func prepareQuery(monitor mntr.Monitor, commit string, firewallEnsurer FirewallE
 			}
 
 			ensureDep := ensureFunc(monitor, conv, curr)
+			reverse(divergentSw)
 			_, err := deriveTraverse(ensureDep, divergentSw)
 			return err
 		}, nil
+	}
+}
+
+func reverse(s []*Dependency) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
 	}
 }
 
