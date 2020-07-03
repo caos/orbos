ec22fac59e8d2a8bc877e5aaec3f5d713d9ae279
Elio Bischof <eliobischof@gmail.com>
2020-07-03 12:52:32 +0200

    fix: disable yum-cron when temporarily unlocking versions
    ✅ Closes: #253

--- internal/operator/nodeagent/dep/package-manager-install.go
+++ internal/operator/nodeagent/dep/package-manager-install.go
@@ -6,20 +6,19 @@ import (
 	"os"
 	"os/exec"
 	"strings"
-	"time"
 
 	"github.com/pkg/errors"
 )
 
 func (p *PackageManager) rembasedInstall(installVersion *Software, more ...*Software) error {
 
-	if err := os.MkdirAll("/etc/yum/pluginconf.d", 0700); err != nil {
+	if err := p.systemd.Disable("yum-cron"); err != nil {
 		return err
 	}
+	defer p.systemd.Enable("yum-cron")
 
-	if err := touch("/etc/yum/pluginconf.d/versionlock.list"); err != nil {
-		return err
-	}
+	errBuf := new(bytes.Buffer)
+	defer errBuf.Reset()
 
 	pkgs := make([]string, len(more)+1)
 	for idx, sw := range append([]*Software{installVersion}, more...) {
@@ -27,15 +26,31 @@ func (p *PackageManager) rembasedInstall(installVersion *Software, more ...*Soft
 		if sw.Version == "" {
 			continue
 		}
-
-		installFmt := fmt.Sprintf("%s-%s", sw.Package, sw.Version)
-		if err := ManipulateFile("/etc/yum/pluginconf.d/versionlock.list", []string{installFmt}, []string{fmt.Sprintf("%s.*", installFmt)}, nil); err != nil {
-			return err
+		pkgs[idx] = fmt.Sprintf("%s-%s", sw.Package, sw.Version)
+		cmd := exec.Command("yum", "versionlock", "delete", sw.Package)
+		cmd.Stderr = errBuf
+		if p.monitor.IsVerbose() {
+			fmt.Println(strings.Join(cmd.Args, " "))
+			cmd.Stdout = os.Stdout
 		}
-	}
+		err := cmd.Run()
+		stderr := errBuf.String()
+		if err != nil && !strings.Contains(stderr, "versionlock delete: no matches") {
+			return errors.Wrapf(err, "unlocking package %s failed with stderr %s", sw.Package, stderr)
+		}
+		errBuf.Reset()
 
-	errBuf := new(bytes.Buffer)
-	defer errBuf.Reset()
+		cmd = exec.Command("yum", "versionlock", "add", "-y", pkgs[idx])
+		cmd.Stderr = errBuf
+		if p.monitor.IsVerbose() {
+			fmt.Println(strings.Join(cmd.Args, " "))
+			cmd.Stdout = os.Stdout
+		}
+		if err := cmd.Run(); err != nil {
+			return errors.Wrapf(err, "locking package %s at version %s failed with stderr %s", sw.Package, sw.Version, errBuf.String())
+		}
+		errBuf.Reset()
+	}
 
 	cmd := exec.Command("yum", append([]string{"install", "-y"}, pkgs...)...)
 	cmd.Stderr = errBuf
@@ -43,24 +58,7 @@ func (p *PackageManager) rembasedInstall(installVersion *Software, more ...*Soft
 		fmt.Println(strings.Join(cmd.Args, " "))
 		cmd.Stdout = os.Stdout
 	}
-	if err := errors.Wrapf(cmd.Run(), "installing yum packages %s failed with stderr %s", strings.Join(pkgs, " and "), errBuf.String()); err != nil {
-		return err
-	}
-	//	errBuf.Reset()
-	//
-	//	cmd := exec.Command("yum", append([]string{"downgrade", "-y"}, pkgs...)...)
-	//	cmd.Stderr = errBuf
-	//	if p.monitor.IsVerbose() {
-	//		fmt.Println(strings.Join(cmd.Args, " "))
-	//		cmd.Stdout = os.Stdout
-	//	}
-	//	err := cmd.Run()
-	//	stdErr := errBuf.String()
-	// TODO: Already installed is no error
-	//	if err != nil {
-	//		return errors.Wrapf(err, "installing yum packages %s failed with stderr %s", strings.Join(pkgs, " and "), stdErr)
-	//	}
-	return nil
+	return errors.Wrapf(cmd.Run(), "installing yum packages %s failed with stderr %s", strings.Join(pkgs, " and "), errBuf.String())
 }
 
 // TODO: Use lower level apt instead of apt-get?
@@ -130,24 +128,6 @@ func (p *PackageManager) debbasedInstall(installVersion *Software, more ...*Soft
 			"package": installVersion.Package,
 			"version": installVersion.Version,
 		}).Debug("Installed package")
-	}
-	return nil
-}
-
-func touch(fileName string) error {
-	_, err := os.Stat(fileName)
-	if os.IsNotExist(err) {
-		file, err := os.Create("temp.txt")
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-	} else {
-		currentTime := time.Now().Local()
-		err = os.Chtimes(fileName, currentTime, currentTime)
-		if err != nil {
-			return err
-		}
 	}
 	return nil
 }
