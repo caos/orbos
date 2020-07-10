98af21077ea98e5a0ca781a421ca257cf56eaf29
Stefan Benz <stefan@caos.ch>
2020-07-10 11:31:03 +0200

    fix(zitadel): added migrations

--- internal/operator/zitadel/kinds/iam/migration/scripts/scripts.go
+++ internal/operator/zitadel/kinds/iam/migration/scripts/scripts.go
@@ -2,8 +2,10 @@ package scripts
 
 func GetAll() map[string]string {
 	return map[string]string{
-		"V1.0__eventstore.sql": V10Eventstore,
-		"V1.1__databases.sql":  V11Databases,
-		"V1.2__views.sql":      V12Views,
+		"V1.0__eventstore.sql":                   V10Eventstore,
+		"V1.1__databases.sql":                    V11Databases,
+		"V1.2__views.sql":                        V12Views,
+		"V1.3__notification_user_loginnames.sql": V13NotificationUserLoginnames,
+		"V1.4__usergrant_grantid.sql":            V14UsergranGrantid,
 	}
 }
