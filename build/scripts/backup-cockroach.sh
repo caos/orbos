3e959256fc1a3be3343650fa6d6b79641e52ae74
Stefan Benz <stefan@caos.ch>
2020-07-28 16:51:55 +0200

    fix(zitadel): corrected variables in backup script

--- build/scripts/backup-cockroach.sh
+++ build/scripts/backup-cockroach.sh
@@ -3,9 +3,11 @@ env=$1
 bucket=$2
 db=$3
 folder=$4
+safile=$5
+certs=$6
+
 filenamelocal=${env}-zitadel-${db}.sql
 filenamebucket=${env}-zitadel-${db}-$(date +"%Y%m%d-%H-%M-%S").sql
 
-/cockroach/cockroach.sh dump --certs-dir=/certificates --host=cockroachdb-public:26257 ${db} > ${filenamelocal}
-curl -H "$(oauth2l header --json sa.json cloud-platform)" -H "Content-Type: application/json" -X POST --data-binary @${filenamelocal} "https://storage.googleapis.com/upload/storage/v1/b/${bucket}/o?uploadType=media&name=${filenamebucket}" > backup.sh
-chmod +x backup.sh
+/cockroach/cockroach.sh dump --certs-dir=${certs} --host=cockroachdb-public:26257 ${db} > ${folder}/${filenamelocal}
+curl -H "$(oauth2l header --json ${safile} cloud-platform)" -H "Content-Type: application/json" -X POST --data-binary @${folder}/${filenamelocal} "https://storage.googleapis.com/upload/storage/v1/b/${bucket}/o?uploadType=media&name=${filenamebucket}"
