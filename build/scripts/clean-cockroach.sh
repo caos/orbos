e352b110e727d2a2da9bec8a49de1e6366dd8ce0
Stefan Benz <stefan@caos.ch>
2020-08-04 14:10:44 +0200

    fix(zitadel): cleanup for user and migration-status

--- build/scripts/clean-cockroach.sh
+++ build/scripts/clean-cockroach.sh
@@ -2,3 +2,5 @@ certs=$1
 db=$2
 
 /cockroach/cockroach.sh sql --certs-dir=${certs} --host=cockroachdb-public:26257 -e "DROP DATABASE IF EXISTS ${db} CASCADE;"
+/cockroach/cockroach.sh sql --certs-dir=${certs} --host=cockroachdb-public:26257 -e "DROP USER IF EXISTS ${db};"
+/cockroach/cockroach.sh sql --certs-dir=${certs} --host=cockroachdb-public:26257 -e "TRUNCATE defaultdb.flyway_history_schema;"
