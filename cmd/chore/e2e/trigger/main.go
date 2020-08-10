30789345133856aa047346694f9d35b62873b22c
Elio Bischof <eliobischof@gmail.com>
2020-08-10 13:34:16 +0200

    chore: pass branch name to trigger

--- cmd/chore/e2e/trigger/main.go
+++ cmd/chore/e2e/trigger/main.go
@@ -11,23 +11,27 @@ import (
 
 func main() {
 
-	var token, org, repository, testcase string
+	var token, org, repository, testcase, branch string
 
 	flag.StringVar(&token, "access-token", "", "Personal access token with repo scope")
 	flag.StringVar(&org, "organization", "", "Github organization")
 	flag.StringVar(&repository, "repository", "", "Github project")
 	flag.StringVar(&testcase, "testcase", "unknown", "Testcase identifier")
+	flag.StringVar(&branch, "branch", "", "Branch to test. Default is current")
 
 	flag.Parse()
 
-	ref, err := exec.Command("git", "branch", "--show-current").Output()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Print("Current Branch: ", string(ref))
+	if branch == "" {
+		ref, err := exec.Command("git", "branch", "--show-current").Output()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Print("Current Branch: ", string(ref))
 
-	branch := strings.TrimPrefix(strings.TrimSpace(string(ref)), "heads/")
-	fmt.Println("Pruned Branch:", branch)
+		branch = strings.TrimPrefix(strings.TrimSpace(string(ref)), "heads/")
+	}
+
+	fmt.Println("Tested branch:", branch)
 	if err := shared.Emit(shared.Event{
 		EventType: "webhook-trigger",
 		ClientPayload: map[string]string{
