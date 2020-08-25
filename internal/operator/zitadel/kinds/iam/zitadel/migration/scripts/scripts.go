81655f95f373b46391eef3b84c9587fabb2ce88b
Stefan Benz <stefan@caos.ch>
2020-08-25 14:39:54 +0200

    fix(zitadel): added migration v1.6

--- internal/operator/zitadel/kinds/iam/zitadel/migration/scripts/scripts.go
+++ internal/operator/zitadel/kinds/iam/zitadel/migration/scripts/scripts.go
@@ -2,11 +2,12 @@ package scripts
 
 func GetAll() map[string]string {
 	return map[string]string{
-		"V1.0__databases.sql":            V10Databases,
-		"V1.1__eventstore.sql":           V11Eventstore,
-		"V1.2__views.sql":                V12Views,
-		"V1.3__usermembership.sql":       V13Usermembership,
-		"V1.4__compliance.sql":           V14Compliance,
-		"V1.5__orgdomain_validationtype": V15OrgDomainValidationType,
+		"V1.0__databases.sql":                V10Databases,
+		"V1.1__eventstore.sql":               V11Eventstore,
+		"V1.2__views.sql":                    V12Views,
+		"V1.3__usermembership.sql":           V13Usermembership,
+		"V1.4__compliance.sql":               V14Compliance,
+		"V1.5__orgdomain_validationtype.sql": V15OrgDomainValidationType,
+		"V1.6__origin_allow_list.sql":        V16OriginAllowList,
 	}
 }
