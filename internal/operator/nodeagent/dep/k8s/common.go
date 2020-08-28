0cb6b88bad6c49fde9d841e275d56e1485b596dc
Elio Bischof <eliobischof@gmail.com>
2020-08-28 10:24:25 +0200

    fix: make rpm repositories world readable
    Aligns available repositories across users. Possibly fixes crashing yum
    dbs.

--- internal/operator/nodeagent/dep/k8s/common.go
+++ internal/operator/nodeagent/dep/k8s/common.go
@@ -52,7 +52,7 @@ baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg`), 0600)
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg`), 0666)
 
 		//		errBuf := new(bytes.Buffer)
 		//		defer errBuf.Reset()
