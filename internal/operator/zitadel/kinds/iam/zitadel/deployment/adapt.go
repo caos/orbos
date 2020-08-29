6fef02d70e9de91fca151df06c281bdb927bf639
Florian Forster <florian@caos.ch>
2020-08-29 11:02:18 +0200

    fix(zitadel): version 0.80.0

--- internal/operator/zitadel/kinds/iam/zitadel/deployment/adapt.go
+++ internal/operator/zitadel/kinds/iam/zitadel/deployment/adapt.go
@@ -216,7 +216,7 @@ func AdaptFunc(
 								RunAsNonRoot: &runAsNonRoot,
 							},
 							Name:            containerName,
-							Image:           "docker.pkg.github.com/caos/zitadel/zitadel:0.78.2",
+							Image:           "docker.pkg.github.com/caos/zitadel/zitadel:0.80.0",
 							ImagePullPolicy: "IfNotPresent",
 							Ports: []corev1.ContainerPort{
 								{Name: "grpc", ContainerPort: 50001},
