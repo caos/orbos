636324d7bf1cd1092cd059d92672be3f43e0966c
Elio Bischof <eliobischof@gmail.com>
2020-09-02 14:23:55 +0200

    fix: unmarshal affinities correctly

--- internal/operator/boom/api/v1beta2/k8s/affinity.go
+++ internal/operator/boom/api/v1beta2/k8s/affinity.go
@@ -1,10 +1,26 @@
 package k8s
 
 import (
+	"encoding/json"
+
 	corev1 "k8s.io/api/core/v1"
 )
 
 type Affinity corev1.Affinity
+
+func (a *Affinity) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	generic := make(map[string]interface{})
+	if err := unmarshal(&generic); err != nil {
+		return err
+	}
+
+	intermediate, err := json.Marshal(generic)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(intermediate, a)
+}
 
 func (a *Affinity) K8s() *corev1.Affinity {
 	if a == nil {
