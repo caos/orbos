0e42042622e0bd4266f3984b1af60746566e5e5c
Elio Bischof <eliobischof@gmail.com>
2020-09-03 14:31:05 +0200

    fix: marshal and unmarshal native k8s types correctly

--- internal/operator/boom/api/v1beta2/k8s/affinity.go
+++ internal/operator/boom/api/v1beta2/k8s/affinity.go
@@ -1,26 +1,10 @@
 package k8s
 
 import (
-	"encoding/json"
-
 	corev1 "k8s.io/api/core/v1"
 )
 
 type Affinity corev1.Affinity
-
-func (a *Affinity) UnmarshalYAML(unmarshal func(interface{}) error) error {
-	generic := make(map[string]interface{})
-	if err := unmarshal(&generic); err != nil {
-		return err
-	}
-
-	intermediate, err := json.Marshal(generic)
-	if err != nil {
-		return err
-	}
-
-	return json.Unmarshal(intermediate, a)
-}
 
 func (a *Affinity) K8s() *corev1.Affinity {
 	if a == nil {
@@ -28,4 +12,12 @@ func (a *Affinity) K8s() *corev1.Affinity {
 	}
 	aff := corev1.Affinity(*a)
 	return &aff
+}
+
+func (a *Affinity) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	return UnmarshalYAML(a, unmarshal)
+}
+
+func (a *Affinity) MarshalYAML() (interface{}, error) {
+	return MarshalYAML(a)
 }
--- internal/operator/boom/api/v1beta2/k8s/resources.go
+++ internal/operator/boom/api/v1beta2/k8s/resources.go
@@ -1,24 +1,15 @@
 package k8s
 
 import (
-	"encoding/json"
-
 	corev1 "k8s.io/api/core/v1"
 )
 
 type Resources corev1.ResourceRequirements
 
 func (r *Resources) MarshalYAML() (interface{}, error) {
-	if r == nil {
-		return nil, nil
-	}
+	return MarshalYAML(r)
+}
 
-	intermediate, err := json.Marshal(r)
-	if err != nil {
-		return nil, err
-	}
-
-	result := make(map[string]interface{})
-
-	return result, json.Unmarshal(intermediate, &result)
+func (r *Resources) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	return UnmarshalYAML(r, unmarshal)
 }
