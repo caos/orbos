05c66b5c2f5274db939c4119600cc6cba49a06c3
Stefan Benz <stefan@caos.ch>
2020-09-08 13:26:21 +0200

    chore(boom): update metrics-server chart 2.11.1 > 2.11.2

--- internal/operator/boom/application/applications/metricsserver/helm/helm.go
+++ internal/operator/boom/application/applications/metricsserver/helm/helm.go
@@ -5,7 +5,7 @@ import "github.com/caos/orbos/internal/operator/boom/templator/helm/chart"
 func GetChartInfo() *chart.Chart {
 	return &chart.Chart{
 		Name:    "metrics-server",
-		Version: "2.11.1",
+		Version: "2.11.2",
 	}
 }
 
