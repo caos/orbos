db0b49bdad837a7a73d915327b167fa0f07fae1a
Stefan Benz <stefan@caos.ch>
2020-09-08 13:27:31 +0200

    chore(boom): update grafana image 7.0.1 > 7.1.5

--- internal/operator/boom/application/applications/grafana/helm/helm.go
+++ internal/operator/boom/application/applications/grafana/helm/helm.go
@@ -5,20 +5,20 @@ import "github.com/caos/orbos/internal/operator/boom/templator/helm/chart"
 func GetChartInfo() *chart.Chart {
 	return &chart.Chart{
 		Name:    "prometheus-operator",
-		Version: "8.13.11",
+		Version: "9.3.1",
 	}
 }
 
 func GetImageTags() map[string]string {
 	return map[string]string{
-		"grafana/grafana":                           "7.0.1",
-		"quay.io/prometheus/alertmanager":           "v0.20.0",
+		"grafana/grafana":                           "7.1.5",
+		"quay.io/prometheus/alertmanager":           "v0.21.0",
 		"squareup/ghostunnel":                       "v1.5.2",
-		"jettech/kube-webhook-certgen":              "v1.2.0",
-		"quay.io/coreos/prometheus-operator":        "v0.38.1",
+		"jettech/kube-webhook-certgen":              "v1.2.1",
+		"quay.io/coreos/prometheus-operator":        "v0.38.3",
 		"quay.io/coreos/configmap-reload":           "v0.0.1",
 		"quay.io/coreos/prometheus-config-reloader": "v0.38.1",
 		"k8s.gcr.io/hyperkube":                      "v1.12.1",
-		"quay.io/prometheus/prometheus":             "v2.17.2",
+		"quay.io/prometheus/prometheus":             "v2.20.1",
 	}
 }
