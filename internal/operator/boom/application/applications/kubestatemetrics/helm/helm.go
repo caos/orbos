9c1caa468f48e368293dd58cc90626bb8251d00e
Stefan Benz <stefan@caos.ch>
2020-09-08 13:27:11 +0200

    chore(boom): update kube-state-metrics chart 2.8.8 > 2.9.1

--- internal/operator/boom/application/applications/kubestatemetrics/helm/helm.go
+++ internal/operator/boom/application/applications/kubestatemetrics/helm/helm.go
@@ -5,12 +5,12 @@ import "github.com/caos/orbos/internal/operator/boom/templator/helm/chart"
 func GetChartInfo() *chart.Chart {
 	return &chart.Chart{
 		Name:    "kube-state-metrics",
-		Version: "2.8.8",
+		Version: "2.9.1",
 	}
 }
 
 func GetImageTags() map[string]string {
 	return map[string]string{
-		"quay.io/coreos/kube-state-metrics": "v1.9.6",
+		"quay.io/coreos/kube-state-metrics": "v1.9.7",
 	}
 }
