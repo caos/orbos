58518c1fca0e04025e06f62e639689824430e353
Stefan Benz <stefan@caos.ch>
2020-09-08 13:25:21 +0200

    chore(boom): update prometheus-node-exporter chart 1.10.0 > 1.11.2

--- internal/operator/boom/application/applications/prometheusnodeexporter/helm/helm.go
+++ internal/operator/boom/application/applications/prometheusnodeexporter/helm/helm.go
@@ -5,12 +5,12 @@ import "github.com/caos/orbos/internal/operator/boom/templator/helm/chart"
 func GetChartInfo() *chart.Chart {
 	return &chart.Chart{
 		Name:    "prometheus-node-exporter",
-		Version: "1.10.0",
+		Version: "1.11.2",
 	}
 }
 
 func GetImageTags() map[string]string {
 	return map[string]string{
-		"quay.io/prometheus/node-exporter": "v1.0.0",
+		"quay.io/prometheus/node-exporter": "v1.0.1",
 	}
 }
