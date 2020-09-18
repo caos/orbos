a237552feb24f8dbf6ea6b2f454eea58cf046952
Livio Amstutz <livio.a@gmail.com>
2020-09-18 10:46:32 +0200

    fix: update zitadel version to 0.86.2

--- internal/operator/zitadel/kinds/iam/zitadel/deployment/adapt.go
+++ internal/operator/zitadel/kinds/iam/zitadel/deployment/adapt.go
@@ -21,7 +21,7 @@ import (
 
 const (
 	//zitadelImage can be found in github.com/caos/zitadel
-	zitadelImage = "ghcr.io/caos/zitadel:0.86.0"	
+	zitadelImage = "ghcr.io/caos/zitadel:0.86.2"	
 )
 
 func AdaptFunc(
