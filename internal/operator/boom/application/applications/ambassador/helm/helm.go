af3960deb46246c6937a08c4b7b9d7f330654e7d
Stefan Benz <stefan@caos.ch>
2020-10-09 11:00:46 +0200

    fix(boom): upgrade ambassador 1.7.0 > 1.8.0

--- internal/operator/boom/application/applications/ambassador/helm/helm.go
+++ internal/operator/boom/application/applications/ambassador/helm/helm.go
@@ -5,7 +5,7 @@ import "github.com/caos/orbos/internal/operator/boom/templator/helm/chart"
 func GetChartInfo() *chart.Chart {
 	return &chart.Chart{
 		Name:    "ambassador",
-		Version: "6.5.3",
+		Version: "6.5.9",
 		Index: &chart.Index{
 			Name: "datawire",
 			URL:  "www.getambassador.io",
@@ -15,7 +15,7 @@ func GetChartInfo() *chart.Chart {
 
 func GetImageTags() map[string]string {
 	return map[string]string{
-		"quay.io/datawire/aes": "1.7.0",
+		"quay.io/datawire/aes": "1.8.0",
 		"prom/statsd-exporter": "v0.8.1",
 	}
 }
