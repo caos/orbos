d899047cea86f748a0068a35b13b020344c6c2d8
Stefan Benz <stefan@caos.ch>
2020-10-20 10:15:20 +0200

    fix(boom): delete metrics-server test pod

--- internal/operator/boom/application/applications/metricsserver/helm.go
+++ internal/operator/boom/application/applications/metricsserver/helm.go
@@ -4,8 +4,18 @@ import (
 	toolsetsv1beta2 "github.com/caos/orbos/internal/operator/boom/api/v1beta2"
 	"github.com/caos/orbos/internal/operator/boom/application/applications/metricsserver/helm"
 	"github.com/caos/orbos/internal/operator/boom/templator/helm/chart"
+	"github.com/caos/orbos/internal/utils/helper"
 	"github.com/caos/orbos/mntr"
 )
+
+func (m *MetricsServer) HelmMutate(monitor mntr.Monitor, toolsetCRDSpec *toolsetsv1beta2.ToolsetSpec, resultFilePath string) error {
+
+	if err := helper.DeleteFirstResourceFromYaml(resultFilePath, "v1", "Pod", "metrics-server-test"); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func (m *MetricsServer) SpecToHelmValues(monitor mntr.Monitor, toolset *toolsetsv1beta2.ToolsetSpec) interface{} {
 	values := helm.DefaultValues(m.GetImageTags())
