2fe597e6a9ca095e9eb6b09af80a2a6d39f51230
Stefan Benz <stefan@caos.ch>
2020-11-02 10:50:28 +0100

    docs(boom): corrected comments in boom.yml

--- internal/operator/boom/api/latest/toolset_types.go
+++ internal/operator/boom/api/latest/toolset_types.go
@@ -17,9 +17,9 @@ type Metadata struct {
 type ToolsetSpec struct {
 	//Boom self reconciling specs
 	Boom *Boom `json:"boom,omitempty" yaml:"boom,omitempty"`
-	//Relative folder path where the currentstate is written to
-	ForceApply bool `json:"forceApply,omitempty" yaml:"forceApply,omitempty"`
 	//Flag if --force should be used by apply of resources
+	ForceApply bool `json:"forceApply,omitempty" yaml:"forceApply,omitempty"`
+	//Relative folder path where the currentstate is written to
 	CurrentStateFolder string `json:"currentStatePath,omitempty" yaml:"currentStatePath,omitempty"`
 	//Spec for the yaml-files applied before the applications, for example used secrets
 	PreApply *Apply `json:"preApply,omitempty" yaml:"preApply,omitempty"`
