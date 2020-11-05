201efdc488b80511a381e1f2f3532a0ebbbf4507
Stefan Benz <stefan@caos.ch>
2020-11-05 10:04:18 +0100

    fix(orbiter): added separate key for root user

--- internal/operator/orbiter/kinds/providers/cs/secrets.go
+++ internal/operator/orbiter/kinds/providers/cs/secrets.go
@@ -22,8 +22,10 @@ func getSecretsMap(desiredKind *Desired) map[string]*secret.Secret {
 	}
 
 	return map[string]*secret.Secret{
-		"apitoken":      desiredKind.Spec.APIToken,
-		"sshkeyprivate": desiredKind.Spec.SSHKey.Private,
-		"sshkeypublic":  desiredKind.Spec.SSHKey.Public,
+		"apitoken":          desiredKind.Spec.APIToken,
+		"sshkeyprivate":     desiredKind.Spec.SSHKey.Private,
+		"sshkeypublic":      desiredKind.Spec.SSHKey.Public,
+		"rootsshkeyprivate": desiredKind.Spec.RootSSHKey.Private,
+		"rootsshkeypublic":  desiredKind.Spec.RootSSHKey.Public,
 	}
 }
