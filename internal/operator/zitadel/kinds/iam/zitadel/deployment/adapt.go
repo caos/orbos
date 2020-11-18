8bfb55ecf63a94cbb7dee21cd307eeed285d5e17
Fabi <38692350+fgerschwiler@users.noreply.github.com>
2020-11-18 17:11:01 +0100

    fix: zitadel version

--- internal/operator/zitadel/kinds/iam/zitadel/deployment/adapt.go
+++ internal/operator/zitadel/kinds/iam/zitadel/deployment/adapt.go
@@ -21,7 +21,7 @@ import (
 
 const (
 	//zitadelImage can be found in github.com/caos/zitadel repo
-	zitadelImage = "ghcr.io/caos/zitadel:0.104.4"
+	zitadelImage = "ghcr.io/caos/zitadel:0.104.5"
 )
 
 func AdaptFunc(
