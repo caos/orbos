f33d0af428c39e5c2a14bc363b8da5e8ebac1689
Elio Bischof <eliobischof@gmail.com>
2020-11-24 23:28:43 +0100

    chore: use new list command

--- scripts/switch_to_orbiter_user.sh
+++ scripts/switch_to_orbiter_user.sh
@@ -2,8 +2,11 @@
 
 set -ex
 
-KEY="${1}"
-IP="${2}"
+ORB="${1}"
+KEY="${2}"
 
-ssh -o StrictHostKeyChecking=no -i ${KEY} root@${IP} 'bash -s' < ./scripts/add_orbiter_user.sh
-ssh -i ${KEY} orbiter@${IP} 'bash -s' < ./scripts/remove_root_access.sh
+for IP in $(orbctl -f ${ORB} nodes list --column ip); do
+	ssh -o StrictHostKeyChecking=no -i ${KEY} root@${IP} 'bash -s' < ./scripts/add_orbiter_user.sh
+	ssh -i ${KEY} orbiter@${IP} 'bash -s' < ./scripts/remove_root_access.sh
+done
+
