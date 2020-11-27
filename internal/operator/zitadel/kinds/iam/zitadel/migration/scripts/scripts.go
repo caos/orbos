fe0fd72b9c9f2cd2e3bab1baf11429500b34c637
Livio Amstutz <livio.a@gmail.com>
2020-11-27 13:52:30 +0100

    add script to migrations

--- internal/operator/zitadel/kinds/iam/zitadel/migration/scripts/scripts.go
+++ internal/operator/zitadel/kinds/iam/zitadel/migration/scripts/scripts.go
@@ -25,5 +25,6 @@ func GetAll() map[string]string {
 		"V1.20__label_policies.sql":          V120LabelPolicies,
 		"V1.21__mfa_policy.sql":              V121MfaPolicy,
 		"V1.22__user_grant_owners.sql":       V122UserGrantOwners,
+		"V1.23__application_view.sql":        V123ApplicationView,
 	}
 }
