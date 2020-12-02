cc168cadb2258b512f5135aa9148c845b76d4b0f
Livio Amstutz <livio.a@gmail.com>
2020-12-02 16:49:04 +0100

    fix: update zitadel to 0.108.0 (webauthn)

--- internal/operator/zitadel/kinds/iam/zitadel/migration/scripts/scripts.go
+++ internal/operator/zitadel/kinds/iam/zitadel/migration/scripts/scripts.go
@@ -27,5 +27,6 @@ func GetAll() map[string]string {
 		"V1.22__user_grant_owners.sql":       V122UserGrantOwners,
 		"V1.23__application_view.sql":        V123ApplicationView,
 		"V1.24__current_sequence.sql":        V124CurrentSequence,
+		"V1.25__webauthn.sql":                V125WebAuthN,
 	}
 }
