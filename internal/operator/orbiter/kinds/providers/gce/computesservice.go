44b79a9be1157ae15a8d684dcb85f99dc29c72f2
Stefan Benz <stefan@caos.ch>
2020-12-09 14:02:12 +0100

    fix(gce): add configurable zonal boot disktype

--- internal/operator/orbiter/kinds/providers/gce/computesservice.go
+++ internal/operator/orbiter/kinds/providers/gce/computesservice.go
@@ -95,6 +95,10 @@ func (m *machinesService) Create(poolName string) (infra.Machine, error) {
 		memoryPerCore = float64(memory) / float64(cores)
 	}
 
+	diskType := ""
+	if desired.StorageDiskType != "" {
+		diskType = fmt.Sprintf("zones/%s/diskTypes/%s", m.context.desired.Zone, desired.StorageDiskType)
+	}
 	disks := []*compute.AttachedDisk{{
 		Type:       "PERSISTENT",
 		AutoDelete: true,
@@ -102,6 +106,7 @@ func (m *machinesService) Create(poolName string) (infra.Machine, error) {
 		InitializeParams: &compute.AttachedDiskInitializeParams{
 			DiskSizeGb:  int64(desired.StorageGB),
 			SourceImage: desired.OSImage,
+			DiskType:    diskType,
 		}},
 	}
 
