3dc30e0d88388a3fb453edc3d81fc45103b6e7de
Elio Bischof <eliobischof@gmail.com>
2020-12-17 10:25:25 +0100

    chore: fix networking-debug.sh

--- scripts/networking-debug.sh
+++ scripts/networking-debug.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-go build -gcflags "all=-N -l" -o /tmp/networking-debug ./cmd/nw-debug
+go build -gcflags "all=-N -l" -o /tmp/nw-debug ./cmd/networking-debug
 dlv exec --api-version 2 --headless --listen 127.0.0.1:2345 /tmp/nw-debug -- "$@"
