1647ad09ee898f083d8d28595ff7280aab913313
Elio Bischof <eliobischof@gmail.com>
2020-12-22 17:17:29 +0100

    refactor: remove senseless direct map from selectable func

--- pkg/labels/selectable.go
+++ pkg/labels/selectable.go
@@ -60,10 +60,6 @@ func (l *Selectable) MarshalYAML() (interface{}, error) {
 	return l.model, nil
 }
 
-func MustForNameAsSelectableK8SMap(l *Component, name string) map[string]string {
-	return MustK8sMap(AsSelectable(MustForName(l, name)))
-}
-
 func MustForNameK8SMap(l *Component, name string) map[string]string {
 	return MustK8sMap(MustForName(l, name))
 }
