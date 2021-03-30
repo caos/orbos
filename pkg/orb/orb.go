188eab47314c06bc7c9ab08379d1815a8f830b4b
Elio Bischof <eliobischof@gmail.com>
2021-03-30 11:29:50 +0200

    refactor: move reconfiguring an orb to the pkg dir

--- pkg/orb/orb.go
+++ pkg/orb/orb.go
@@ -1,11 +1,18 @@
 package orb
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 
-	secret2 "github.com/caos/orbos/pkg/secret"
+	"github.com/caos/orbos/internal/ssh"
+	"github.com/caos/orbos/internal/stores/github"
+	"github.com/caos/orbos/mntr"
+	"github.com/caos/orbos/pkg/git"
+
+	"github.com/caos/orbos/pkg/secret"
 
 	"github.com/caos/orbos/internal/helpers"
 
@@ -73,15 +80,95 @@ func ParseOrbConfig(orbConfigPath string) (*Orb, error) {
 	}
 
 	orb.Path = orbConfigPath
-	secret2.Masterkey = orb.Masterkey
+	secret.Masterkey = orb.Masterkey
 	return orb, nil
 }
 
-func (o *Orb) WriteBackOrbConfig() error {
+func (o *Orb) writeBackOrbConfig() error {
 	data, err := yaml.Marshal(o)
 	if err != nil {
 		return err
 	}
 
 	return ioutil.WriteFile(o.Path, data, os.ModePerm)
+}
+
+func ReconfigureAndClone(ctx context.Context, monitor mntr.Monitor, orbConfig *Orb, newRepoURL, newMasterKey string, gitClient *git.Client) error {
+	if orbConfig.URL == "" && newRepoURL == "" {
+		return errors.New("repository url is neighter passed by flag repourl nor written in orbconfig")
+	}
+
+	// TODO: Remove?
+	if orbConfig.URL != "" && newRepoURL != "" && orbConfig.URL != newRepoURL {
+		return fmt.Errorf("repository url %s is not reconfigurable", orbConfig.URL)
+	}
+
+	if orbConfig.Masterkey == "" && newMasterKey == "" {
+		return errors.New("master key is neighter passed by flag masterkey nor written in orbconfig")
+	}
+
+	var changes bool
+	if newMasterKey != "" {
+		monitor.Info("Changing masterkey in current orbconfig")
+		if orbConfig.Masterkey == "" {
+			secret.Masterkey = newMasterKey
+		}
+		orbConfig.Masterkey = newMasterKey
+		changes = true
+	}
+	if newRepoURL != "" {
+		monitor.Info("Changing repository url in current orbconfig")
+		orbConfig.URL = newRepoURL
+		changes = true
+	}
+
+	configureGit := func() error {
+		return gitClient.Configure(orbConfig.URL, []byte(orbConfig.Repokey))
+	}
+
+	// If the repokey already has read/write permissions, don't generate a new one.
+	// This ensures git providers other than github keep being supported
+	if err := configureGit(); err != nil {
+
+		monitor.Info("Starting connection with git-repository")
+
+		dir := filepath.Dir(orbConfig.Path)
+
+		deployKeyPrivLocal, deployKeyPub, err := ssh.Generate()
+		if err != nil {
+			panic(errors.New("failed to generate ssh key for deploy key"))
+		}
+		g := github.New(monitor).LoginOAuth(ctx, dir)
+		if g.GetStatus() != nil {
+			return errors.New("failed github oauth login ")
+		}
+		repo, err := g.GetRepositorySSH(orbConfig.URL)
+		if err != nil {
+			return errors.New("failed to get github repository")
+		}
+
+		if err := g.EnsureNoDeployKey(repo).GetStatus(); err != nil {
+			monitor.Error(errors.New("failed to clear deploy keys in repository"))
+		}
+
+		if err := g.CreateDeployKey(repo, deployKeyPub).GetStatus(); err != nil {
+			return errors.New("failed to create deploy keys in repository")
+		}
+		orbConfig.Repokey = deployKeyPrivLocal
+
+		if err := configureGit(); err != nil {
+			return err
+		}
+		changes = true
+	}
+
+	if changes {
+		monitor.Info("Writing local orbconfig")
+		if err := orbConfig.writeBackOrbConfig(); err != nil {
+			monitor.Info("Failed to change local configuration")
+			return err
+		}
+	}
+
+	return gitClient.Clone()
 }
