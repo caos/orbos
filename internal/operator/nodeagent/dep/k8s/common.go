d7b28213761e16619ffa0f6d56171418793833d9
Elio Bischof <eliobischof@gmail.com>
2021-04-30 14:37:10 +0200

    fix: install kubernetes packages

--- internal/operator/nodeagent/dep/k8s/common.go
+++ internal/operator/nodeagent/dep/k8s/common.go
@@ -46,21 +46,26 @@ func (c *Common) Ensure(remove common.Package, install common.Package, leaveOSRe
 	if !leaveOSRepositories {
 		switch c.os {
 		case dep.Ubuntu:
-			return c.manager.Add(&dep.Repository{
+			if err := c.manager.Add(&dep.Repository{
 				KeyURL:         "https://packages.cloud.google.com/apt/doc/apt-key.gpg",
 				KeyFingerprint: "",
 				Repository:     "deb https://apt.kubernetes.io/ kubernetes-xenial main",
-			})
+			}); err != nil {
+				return err
+			}
 		case dep.CentOS:
-			return ioutil.WriteFile("/etc/yum.repos.d/kubernetes.repo", []byte(`[kubernetes]
+			if err := ioutil.WriteFile("/etc/yum.repos.d/kubernetes.repo", []byte(`[kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg`), 0600)
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg`), 0600); err != nil {
+				return err
+			}
+		default:
+			return errors.New("unknown OS")
 		}
-		return errors.New("unknown OS")
 	}
 
 	pkgVersion := strings.TrimLeft(install.Version, "v") + "-0"
