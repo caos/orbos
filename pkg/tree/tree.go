a44d7cd5da1d5d394a6cb457dd01214e8a16eaf6
Elio Bischof <eliobischof@gmail.com>
2021-07-19 17:26:30 +0200

    fix: make resource version and resource apiVersion compatible

--- pkg/tree/tree.go
+++ pkg/tree/tree.go
@@ -16,7 +16,36 @@ type Tree struct {
 
 type Common struct {
 	Kind    string `json:"kind" yaml:"kind"`
-	Version string `json:"apiVersion" yaml:"apiVersion"`
+	Version string `json:"version, omitempty" yaml:"version, omitempty"`
+	// Don't access X_ApiVersion, it is only exported for (de-)serialization
+	X_ApiVersion     string `json:"apiVersion, omitempty" yaml:"apiVersion, omitempty"`
+	parsedApiVersion bool   `json: "-" yaml: "-"`
+}
+
+func (c *Common) UnmarshalYAML(node *yaml.Node) error {
+	type proxy Common
+	p := proxy{}
+	if err := node.Decode(&p); err != nil {
+		return err
+	}
+	c.Kind = p.Kind
+	c.Version = p.Version
+	c.X_ApiVersion = p.X_ApiVersion
+	if c.Version == "" && c.X_ApiVersion != "" {
+		c.Version = c.X_ApiVersion
+		c.parsedApiVersion = true
+	}
+	return nil
+}
+
+func (c *Common) MarshalYAML() (interface{}, error) {
+	if c.parsedApiVersion {
+		clone := new(Common)
+		clone.Kind = c.Kind
+		clone.X_ApiVersion = c.Version
+		return clone, nil
+	}
+	return c, nil
 }
 
 func (c *Tree) UnmarshalYAML(node *yaml.Node) error {
@@ -24,9 +53,10 @@ func (c *Tree) UnmarshalYAML(node *yaml.Node) error {
 	*c.Original = *node
 
 	c.Common = new(Common)
-	if err := c.Original.Decode(c.Common); err != nil || c.Common.Version == "" || c.Common.Kind == "" {
+	if err := c.Original.Decode(c.Common); err != nil {
 		return mntr.ToUserError(fmt.Errorf("decoding version or kind failed: kind \"%s\", version \"%s\", err %w", c.Common.Kind, c.Common.Version, err))
 	}
+
 	return nil
 }
 
