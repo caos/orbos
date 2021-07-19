4baccdb9e0b88209c23907d6ad4ee88c32f35fa1
Elio Bischof <eliobischof@gmail.com>
2021-07-19 16:58:40 +0200

    fix: apiVersion instead of version

--- pkg/tree/tree.go
+++ pkg/tree/tree.go
@@ -15,8 +15,8 @@ type Tree struct {
 }
 
 type Common struct {
-	Kind    string `json:"kind"`
-	Version string `json:"version" yaml:"version" yaml:"apiVersion"`
+	Kind    string `json:"kind" yaml:"kind"`
+	Version string `json:"apiVersion" yaml:"apiVersion"`
 }
 
 func (c *Tree) UnmarshalYAML(node *yaml.Node) error {
@@ -25,7 +25,7 @@ func (c *Tree) UnmarshalYAML(node *yaml.Node) error {
 
 	c.Common = new(Common)
 	if err := c.Original.Decode(c.Common); err != nil || c.Common.Version == "" || c.Common.Kind == "" {
-		return mntr.ToUserError(fmt.Errorf("decoding version or kind failed: kind \"%s\", version \"%s\", err %w", c.Common.Version, c.Common.Kind, err))
+		return mntr.ToUserError(fmt.Errorf("decoding version or kind failed: kind \"%s\", version \"%s\", err %w", c.Common.Kind, c.Common.Version, err))
 	}
 	return nil
 }
