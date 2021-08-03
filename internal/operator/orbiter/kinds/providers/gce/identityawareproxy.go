905668247c09eca41d9d85ba8e73a5cc4af5f0e2
Stefan Benz <stefan@caos.ch>
2021-08-03 11:05:17 +0200

    fix: don't ensure IAP in gce provider

--- internal/operator/orbiter/kinds/providers/gce/identityawareproxy.go
+++ internal/operator/orbiter/kinds/providers/gce/identityawareproxy.go
@@ -1,11 +1,12 @@
 package gce
 
 import (
+	"google.golang.org/api/googleapi"
 	"google.golang.org/api/servicemanagement/v1"
 )
 
 func ensureIdentityAwareProxyAPIEnabled(c *context) error {
-	svc, err := servicemanagement.NewService(c.ctx, *c.auth)
+	_, err := servicemanagement.NewService(c.ctx, *c.auth)
 	if err != nil {
 		return err
 	}
@@ -14,13 +15,19 @@ func ensureIdentityAwareProxyAPIEnabled(c *context) error {
 		func() {
 			c.monitor.Debug("Enabling Identity Aware Proxy API")
 		},
-		servicesOpCall(svc.Services.Create(&servicemanagement.ManagedService{
+
+		servicesOpCall(create),
+		/*svc.Services.Create(&servicemanagement.ManagedService{
 			ServiceName:       "iap.googleapis.com",
 			ProducerProjectId: c.projectID,
-		}).Do),
+		}).Do*/
 		func() error {
 			c.monitor.Debug("Identity Aware Proxy API ensured")
 			return nil
 		},
 	)()
+}
+
+func create(opts ...googleapi.CallOption) (*servicemanagement.Operation, error) {
+	return nil, nil
 }
